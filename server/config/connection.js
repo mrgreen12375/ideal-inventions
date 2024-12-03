6d38aab49e8ba3226b87a731eb2058d62aa68b10
Steven Green <stevengreen@Stevens-MacBook-Pro.local>
2024-12-02 22:28:55 -0700

    reverted back to old connection string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,5 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/inventionsDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/inventionsDB');
 
 module.exports = mongoose.connection;
